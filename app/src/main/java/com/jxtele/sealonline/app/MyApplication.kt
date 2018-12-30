aa0322b200189428cfaf992510c356b7a32bdc40
ZealP <ZealP@github.com>
2018-12-30 20:37:28 +0800

    测试git

--- app/src/main/java/com/jxtele/sealonline/app/MyApplication.kt
+++ app/src/main/java/com/jxtele/sealonline/app/MyApplication.kt
@@ -13,6 +13,7 @@ class MyApplication : Application() {
     companion object {
         lateinit var application: MyApplication
     }
+
     override fun onCreate() {
         super.onCreate()
         application = this
